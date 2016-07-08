d137cb9d3641fd520b0460741edd030580fd97e2
Felipe Cardozo <dafelcardozo@gmail.com>
2016-07-08 14:02:55 -0500

    node_modules folder set as gitignored.

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+//Lets define a port we want to listen to
+const PORT=8080;
 
-// Serve up public/ftp folder
-var serve = serveStatic('.', {'index': ['index.html', 'index.htm']})
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+  try {
+     //log the request on console
+     console.log(request.url);
+     //Disptach
+     dispatcher.dispatch(request, response);
+ } catch(err) {
+     console.log(err);
+ }
+}
 
-// Create server
-var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, finalhandler(req, res))
-})
+//Create a server
+var server = http.createServer(handleRequest);
 
-// Listen
-server.listen(80)
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('.');
+
+//A sample GET request
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});
