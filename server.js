fdf3fcdea3552a6018717c9c9a769191bd38e26d
Felipe Cardozo <dafelcardozo@gmail.com>
2016-07-08 15:46:25 -0500

    node_modules dependencies added again.

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE 'html'>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
 });
 
 server.listen(80);
-console.log("Server is listening");
